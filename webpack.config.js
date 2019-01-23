f9bfbba23a81ef32946af8d865af45928507d9b3
Heiko Beineke <heiko.beineke@gmail.com>
2019-01-23 23:51:57 +0100

    Integrate Bulma

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,10 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: ['babel-loader']
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
       }
     ]
   },
